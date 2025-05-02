29c1f7d2ac659243709f56dfd7fbbf492ea98b3e
francosegura <tu-email@ejemplo.com>
2025-05-02 12:07:36 -0300

    refactor: Visual refactor

--- context/TaskContext.tsx
+++ context/TaskContext.tsx
@@ -25,7 +25,7 @@ export function TaskProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (user) {
-      loadTasks();
+    //   loadTasks();
     }
   }, [user]);
 
